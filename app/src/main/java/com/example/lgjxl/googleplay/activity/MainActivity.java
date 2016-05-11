828b73fcf3253757d80e321003baf206dfc5240a
liujun <lgjxlj@126.com>
2016-05-11 21:46:59 +0800

    添加ToolBar，添加drawerlayout

--- app/src/main/java/com/example/lgjxl/googleplay/activity/MainActivity.java
+++ app/src/main/java/com/example/lgjxl/googleplay/activity/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.lgjxl.googleplay.activity;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
 
 import com.example.lgjxl.googleplay.R;
 import com.example.lgjxl.googleplay.activity.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -22,6 +24,11 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initToolBar() {
+        mToolbar = (Toolbar) findViewById(R.id.tb_normal);
+        mToolbar.setTitle("GooglePlay");
+        setSupportActionBar(mToolbar);
+        mToolbar.setLogo(R.mipmap.ic_launcher);
+        mToolbar.setNavigationIcon(R.drawable.ic_drawer_am);
 
     }
 

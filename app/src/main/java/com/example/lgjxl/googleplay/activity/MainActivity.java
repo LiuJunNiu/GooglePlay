8b56b946576f2b4f8088fd7bce029750dcd7cb54
liujun <lgjxlj@126.com>
2016-06-12 00:02:30 +0800

    设置好toolbar

--- app/src/main/java/com/example/lgjxl/googleplay/activity/MainActivity.java
+++ app/src/main/java/com/example/lgjxl/googleplay/activity/MainActivity.java
@@ -2,35 +2,44 @@ package com.example.lgjxl.googleplay.activity;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.lgjxl.googleplay.R;
-import com.example.lgjxl.googleplay.activity.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void initView() {
         setContentView(R.layout.activity_main);
-        DrawerLayout drawerLayout = new DrawerLayout(this);
+        initView();
+        addEvents();
     }
 
-    @Override
-    protected void initToolBar() {
+    private void addEvents() {
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+    }
+
+    private void initView() {
         mToolbar = (Toolbar) findViewById(R.id.tb_normal);
         mToolbar.setTitle("GooglePlay");
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main);
+
         setSupportActionBar(mToolbar);
-        mToolbar.setLogo(R.mipmap.ic_launcher);
-        mToolbar.setNavigationIcon(R.drawable.ic_drawer_am);
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,
+                R.string.drawer_open,R.string.drawer_close);
+        mDrawerToggle.syncState();
 
     }
+
 
 
 }
